a6c50bda85635c9eccffb55ca213e55256efa9e2
darshanpawar05 <darshan.pawar23@vit.edu>
2026-04-19 17:14:12 +0530

    Update taxCalculator.js

--- taxCalculator.js
+++ taxCalculator.js
@@ -1,10 +1,6 @@
 function calculateTax() {
-  const income = document.getElementById("income").value;
-  let tax = 0;
+    let income = document.getElementById("income").value;
+    let tax = income * 0.1;
 
-  if (income <= 500000) tax = 0;
-  else if (income <= 1000000) tax = income * 0.1;
-  else tax = income * 0.2;
-
-  document.getElementById("result").innerText = "Tax: ₹" + tax;
+    document.getElementById("result").innerText = "₹" + tax;
 }
